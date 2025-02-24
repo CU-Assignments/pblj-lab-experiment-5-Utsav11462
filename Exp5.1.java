3c6cacae1d721c5471760f8ab381add28d4e2f29
Utsav11462 <120674052+Utsav11462@users.noreply.github.com>
2025-02-24 15:40:50 +0530

    Update Exp5.1.java

--- Exp5.1.java
+++ Exp5.1.java
@@ -15,7 +15,33 @@ Java Program:
 parseStringToInteger(): This method parses a string into an Integer. It catches any NumberFormatException if the string is not a valid number.
 calculateSum(): This method calculates the sum of a list of integers. Java automatically performs unboxing when adding Integer values to sum (an int).
 
+CODE:
+  import java.util.ArrayList;
+  import java.util.List;
 
+public class Main {
+    public static Integer parseStringToInteger(String str) {
+        return Integer.parseInt(str); 
+    }
+    public static int calculateSum(List<Integer> numbers) {
+        int sum = 0;
+        
+        for (Integer num : numbers) {
+            sum += num; 
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40); 
+        numbers.add(parseStringToInteger("50"));
+        int sum = calculateSum(numbers);
+        System.out.println("The sum of the list is: " + sum);
+    }
+}
 
 
 Test Cases:
